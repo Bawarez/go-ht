f47ebcbc8b488935717ecdc1ba840985f95a566f
Dzmitry Ryzhyk <bavarets@tut.by>
2019-02-24 13:45:29 +0300

    trololo

--- task2/main.go
+++ task2/main.go
@@ -1,9 +1,27 @@
 package main
 
-// Specify MapTo function here
+func MapTo(numbers []int, f func(element int, index int) string) []string {
+	var result []string
+	
+	for i, number := range numbers {
+		result = append(result, f(number, i))
+	}
+	return result;
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	values := [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	var result []string; 
+	
+	for _, element := range arr{
+		switch {
+			case (element >= 0) && (element < 10) :
+				result = append(result, values[element])
+			default:
+				result = append(result, "unknown")
+		}
+	}
+	return result;
 }
 
 func main() {
